c13211b4a7b7b05060b9edc2631a649535793629
mdnoorulain <mdnoorulain786.mna@gmail.com>
2024-09-27 18:51:16 +0530

    s commit

--- src/Components/TextChange.jsx
+++ src/Components/TextChange.jsx
@@ -1,33 +1,39 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
+
 const TextChange = () => {
-  const texts = ["Hi, I'm Asmat Najreen", "Hi, I'm Asmat Najreen", "Hi, I'm Asmat Najreen"];
-  const [currenText, setCurrentText] = useState("");
-  const [endValue, setendValue] = useState(true);
+  // Moved texts inside the useEffect
+  const [currentText, setCurrentText] = useState("");
+  const [endValue, setEndValue] = useState(0);
   const [isForward, setIsForward] = useState(true);
   const [index, setIndex] = useState(0);
 
   useEffect(() => {
+    const texts = ["Hi, I'm Asmat Najreen", "Hi, I'm Asmat Najreen", "Hi, I'm Asmat Najreen"];
+    
     const intervalId = setInterval(() => {
-      setCurrentText(texts[index].substring(0, endValue));
+      const currentFullText = texts[index];
+      setCurrentText(currentFullText.substring(0, endValue));
+
       if (isForward) {
-        setendValue((prev) => prev + 1);
+        if (endValue < currentFullText.length) {
+          setEndValue((prev) => prev + 1);
+        } else {
+          setIsForward(false);
+        }
       } else {
-        setendValue((prev) => prev - 1);
-      }
-      if (endValue > texts[index].length + 10) {
-        setIsForward(false);
-      }
-      if (endValue < 2.1) {
-        setIsForward(true);
-        setIndex((prev) => prev & texts.length);
+        if (endValue > 0) {
+          setEndValue((prev) => prev - 1);
+        } else {
+          setIsForward(true);
+          setIndex((prev) => (prev + 1) % texts.length);
+        }
       }
     }, 50);
 
     return () => clearInterval(intervalId);
-  }, [endValue, isForward, index, texts]);
+  }, [endValue, isForward, index]);
 
-  return <div className="transition ease duration-300">{currenText}</div>;
+  return <div className="transition ease duration-300">{currentText}</div>;
 };
 
 export default TextChange;
